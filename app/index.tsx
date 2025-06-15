cb8eac1d68ac0d384031ea215fe712cfd6ed7770
Jiksi <zhicxiap99@gmail.com>
2025-06-16 01:18:47 +0800

    feat: update layout styles in Index component for improved responsiveness

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import EventSection from "@/components/event-section";
 import Header from "@/components/header";
 import HeroSection from "@/components/hero-section";
-import { colors } from "@/styles";
+import { colors, container } from "@/styles";
 import { ScrollView, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
@@ -9,15 +9,8 @@ export default function Index() {
   return (
     <SafeAreaProvider style={{ backgroundColor: colors.background }}>
       <SafeAreaView style={{ flex: 1 }}>
-        <ScrollView style={{}}>
-          <View
-            style={{
-              maxWidth: 640,
-              paddingHorizontal: 32,
-              marginHorizontal: "auto",
-              gap: 24,
-            }}
-          >
+        <ScrollView>
+          <View style={container.main}>
             <Header />
             <HeroSection />
             <EventSection />
