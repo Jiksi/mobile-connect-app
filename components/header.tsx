cb4d68f2f4861bd4ce7210c56d8adfe1d630247a
Jiksi <zhicxiap99@gmail.com>
2025-06-16 09:21:35 +0800

    feat: refactor Header component for improved structure and state management

--- components/header.tsx
+++ components/header.tsx
@@ -1,32 +1,44 @@
+import NavMenu from "@/components/nav-menu";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { Image, Text, View } from "react-native";
+import { useState } from "react";
+import { Image, Pressable, Text, View } from "react-native";
 
 export default function Header() {
+  const [isNavMenuVisible, setIsNavMenuVisible] = useState(false);
+
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        paddingVertical: 16,
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <View
         style={{
           flexDirection: "row",
+          paddingVertical: 16,
+          justifyContent: "space-between",
           alignItems: "center",
-          gap: 16,
         }}
       >
-        <Image
-          style={{ width: 32, height: 32 }}
-          source={require("@/assets/images/manus-logo.png")}
-        />
-        <Text style={{ fontSize: 16, fontWeight: "bold" }}>manus</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 16,
+          }}
+        >
+          <Image
+            style={{ width: 32, height: 32 }}
+            source={require("@/assets/images/manus-logo.png")}
+          />
+          <Text style={{ fontSize: 16, fontWeight: "bold" }}>manus</Text>
+        </View>
+        <View>
+          <Pressable onPress={() => setIsNavMenuVisible(true)}>
+            <Ionicons name="menu" size={24} color="black" />
+          </Pressable>
+        </View>
       </View>
-      <View>
-        <Ionicons name="menu" size={24} color="black" />
-      </View>
+      <NavMenu
+        isVisible={isNavMenuVisible}
+        onClose={() => setIsNavMenuVisible(false)}
+      />
     </View>
   );
 }
