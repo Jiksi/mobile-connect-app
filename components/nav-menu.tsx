4f2f7c4a5656697e77a53d66398277e24fa1c12f
Jiksi <zhicxiap99@gmail.com>
2025-06-20 18:00:08 +0800

    feat: update styling for modal navigation

--- components/nav-menu.tsx
+++ components/nav-menu.tsx
@@ -1,5 +1,5 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { Image, Modal, Pressable, Text, View } from "react-native";
+import { Image, Modal, Pressable, ScrollView, Text, View } from "react-native";
 
 type Props = {
   isVisible: boolean;
@@ -10,35 +10,53 @@ export default function NavMenu({ isVisible, onClose }: Props) {
   return (
     <View>
       <Modal animationType="slide" visible={isVisible}>
-        <View style={{ paddingHorizontal: 32 }}>
+        <ScrollView>
           <View
             style={{
-              flexDirection: "row",
-              paddingVertical: 16,
-              justifyContent: "space-between",
+              height: "100%",
+              width: "100%",
               alignItems: "center",
             }}
           >
             <View
               style={{
-                flexDirection: "row",
-                alignItems: "center",
-                gap: 16,
+                width: "100%",
+                maxWidth: 640,
+                paddingHorizontal: 32,
               }}
             >
-              <Image
-                style={{ width: 32, height: 32 }}
-                source={require("@/assets/images/manus-logo.png")}
-              />
-              <Text style={{ fontSize: 16, fontWeight: "bold" }}>manus</Text>
-            </View>
-            <View>
-              <Pressable onPress={onClose}>
-                <Ionicons name="close" size={24} color="black" />
-              </Pressable>
+              <View
+                style={{
+                  flexDirection: "row",
+                  paddingVertical: 16,
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    gap: 16,
+                  }}
+                >
+                  <Image
+                    style={{ width: 32, height: 32 }}
+                    source={require("@/assets/images/manus-logo.png")}
+                  />
+                  <Text style={{ fontSize: 16, fontWeight: "bold" }}>
+                    manus
+                  </Text>
+                </View>
+                <View>
+                  <Pressable onPress={onClose}>
+                    <Ionicons name="close" size={24} color="black" />
+                  </Pressable>
+                </View>
+              </View>
             </View>
           </View>
-        </View>
+        </ScrollView>
       </Modal>
     </View>
   );
