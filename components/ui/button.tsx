043aaca8bf058bc84da7095c2d4e3e9f4bff6cf9
Jiksi <zhicxiap99@gmail.com>
2025-06-21 17:47:49 +0800

    feat: enhance Button component to support disabled state with opacity adjustment

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,18 +5,24 @@ import { Pressable, Text, View } from "react-native";
 type Props = PropsWithChildren<{
   onPress?: () => void;
   variant?: "primary" | "secondary";
+  disabled?: boolean;
 }>;
 
 export default function Button({
   children,
   onPress,
   variant = "primary",
+  disabled = false,
 }: Props) {
   return (
     <View>
       <Pressable
-        style={variant === "primary" ? button.primary : button.secondary}
+        style={[
+          variant === "primary" ? button.primary : button.secondary,
+          { opacity: disabled ? 0.5 : 1 },
+        ]}
         onPress={onPress}
+        disabled={disabled}
       >
         <Text
           style={[
@@ -26,6 +32,7 @@ export default function Button({
                 variant === "primary"
                   ? colors.primaryForeground
                   : colors.secondaryForeground,
+              opacity: disabled ? 0.5 : 1,
             },
           ]}
         >
